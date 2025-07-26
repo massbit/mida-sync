85a800da554d6fb789bfdab12da33c83a3a991ab
Nico Montanari <nico.montanari@22hbg.it>
2025-07-26 20:32:44 +0200

    fix: update database host in production configuration to use host.docker.internal

--- src/config/production.ts
+++ src/config/production.ts
@@ -3,7 +3,7 @@ import { Config } from './config'
 export const productionConfig: Config = {
     database: {
         user: 'postgres',
-        host: 'localhost',
+        host: 'host.docker.internal',
         database: 'mida',
         password: 'gAJqPYHM8p7WRLrWZFCj',
         port: 5432,
